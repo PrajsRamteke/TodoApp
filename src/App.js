12b469efb7c599e0c33d0c4012d5de06e1ac3d35
Prajwal <prajwalramteke9@gmail.com>
2023-02-15 15:11:50 +0530

    todo app update add localstorage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import './App.css';
-import TodoApp from './components/TodoApp';
+import "./App.css";
+import TodoApp from "./components/TodoApp";
 
 function App() {
   return (
     <div className="App">
-    <TodoApp/>
+      <TodoApp />
     </div>
   );
 }
--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -1,8 +1,19 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function TodoApp() {
   const [todo, setTodo] = useState("");
   const [data, setData] = useState([]);
+
+  // useEffect(() => {
+  //   const storeData = JSON.parse(localStorage.getItem("todos"));
+  //   if (storeData) {
+  //     setData(storeData);
+  //   }
+  // }, []);
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(data));
+  }, [data]);
 
   function AddTodo() {
     setData([...data, todo]);
@@ -10,32 +21,44 @@ function TodoApp() {
     // console.log(data);
   }
   const Removeitem = (index) => {
-    let arr = data;
-    data.splice(index, 1);
-    setData([...arr]);
+    const newData = [...data];
+    newData.splice(index, 1);
+    setData(newData);
   };
   function RemoveAll() {
     setData([]);
+    // localStorage.removeItem("todos");
+    localStorage.clear();
   }
   return (
     <>
       <div className="Background">
         <div className="main">
-            <h1>Todo App</h1>
-            <div className="user">
-              <input type="text" value={todo} onChange={(event) => setTodo(event.target.value)} />
-              <button onClick={AddTodo}>Add</button>
-            </div>
-            <div className="heading">
-              <h3>Your Data</h3>
-              <h3 className="RemoveAll" onClick={RemoveAll}>Remove</h3>
-            </div>
+          <h1>Todo App</h1>
+          <div className="user">
+            <input
+              type="text"
+              value={todo}
+              onChange={(event) => setTodo(event.target.value)}
+            />
+            <button onClick={AddTodo}>Add</button>
+          </div>
+          <div className="heading">
+            <h3>Your Data</h3>
+            <h3 className="RemoveAll" onClick={RemoveAll}>
+              Remove
+            </h3>
+          </div>
           <div>
             {data.map((element, index) => {
               return (
                 <div key={index} className="todo_data">
                   <h2>{element}</h2>
-                  <div> <button className="btn" onClick={() => Removeitem(index)}>Remove</button> </div>
+                  <div>
+                    <button className="btn" onClick={() => Removeitem(index)}>
+                      Remove
+                    </button>
+                  </div>
                 </div>
               );
             })}
